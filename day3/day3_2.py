6bf6fe910f7381127f0f62e6c2d68b75b4b59f4d
ngvlz <linhvunguyen258@gmail.com>
2020-12-04 00:17:24 -0500

    Add Day 3 Puzzle 2 solution

---
+++ day3/day3_2.py
@@ -0,0 +1,34 @@
+slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+
+def tree_encountered(slope_x, slope_y):
+    map = []
+    x = 0
+    tree = 0
+    with open('input') as _input:
+        for line in _input:
+            line = line.rstrip("\n")
+            square = list(line)
+            map.append(square)
+    map = map[::slope_y]
+    
+    for row in map:
+        if row[x] == "#":
+            tree+=1 
+        x+=slope_x
+        if x >= len(row):
+            x = x - len(row)
+    return(tree)
+
+def multiply(num_list):
+    result = 1
+    for num in num_list:
+        result *= num
+    return result
+
+tree_list = []
+
+for slope in slopes:
+    tree_per_slope = tree_encountered(slope[0], slope[1])
+    tree_list.append(tree_per_slope)
+
+print(multiply(tree_list))
