127cedffe838d821ac82b4fefd32e2fc2cfe546c
ngvlz <linhvunguyen258@gmail.com>
2020-12-05 14:54:47 -0500

    Upload Day 5 puzzles solution

---
+++ day5/day5_1.py
@@ -0,0 +1,44 @@
+with open('input', 'r') as input:
+    # bp = Boarding Pass
+    bp_data = input.read() 
+
+bp_list = bp_data.split('\n')
+
+# convert character into seat position
+def get_seat_pos(char, id_list):
+    middle_idx = len(id_list)//2
+    first_half = id_list[:middle_idx]
+    second_half = id_list[middle_idx:]
+    if char == 'F' or char == 'L':
+        result = first_half
+    else: #char == 'B' or char == 'R'
+        result = second_half
+    return result
+
+# get seat ID from input string
+# print result of each ID to the screen
+def get_seat_id(bp):
+    print('-----------------')
+    print(bp)
+    for i in range(len(bp)):
+        if i <= 6:
+            exec(f"r{i+1} = get_seat_pos(bp[{i}], r{i})")
+            exec(f"if {i} == 6: print('Row:   ', r{i+1})")
+        if i > 6:
+            exec(f"c{i-6} = get_seat_pos(bp[{i}], c{i-7})")
+            exec(f"if {i} == 9: print('Column:',c{i-6})")
+        if i == 9:
+            exec(f"id = r{i-2}[0]*8 + c{i-6}[0]")
+            exec(f"if {i} == 9: print('Seat ID:',id)")
+    return exec(f"seat_ids.append(id)")
+
+r0 = list(range(128))
+c0 = list(range(8))
+seat_ids = []
+
+for bp in bp_list:
+    get_seat_id(bp)
+
+# Final answer
+print('-----------------\n'
+    + 'Max seat ID:',max(seat_ids))
---
+++ day5/day5_2.py
@@ -0,0 +1,42 @@
+with open('input', 'r') as input:
+    # bp = Boarding Pass
+    bp_data = input.read() 
+
+bp_list = bp_data.split('\n')
+
+# convert character into seat position
+def get_seat_pos(char, id_list):
+    middle_idx = len(id_list)//2
+    first_half = id_list[:middle_idx]
+    second_half = id_list[middle_idx:]
+    if char == 'F' or char == 'L':
+        result = first_half
+    else: #char == 'B' or 'R'
+        result = second_half
+    return result
+
+def get_seat_id(bp):
+    for i in range(len(bp)):
+        if i <= 6:
+            exec(f"r{i+1} = get_seat_pos(bp[{i}], r{i})")
+        if i > 6:
+            exec(f"c{i-6} = get_seat_pos(bp[{i}], c{i-7})")
+        if i == 9:
+            exec(f"id = r{i-2}[0]*8 + c{i-6}[0]")
+    exec(f"seat_ids.append(id)")
+
+r0 = list(range(128))
+c0 = list(range(8))
+seat_ids = []
+
+for bp in bp_list:
+    get_seat_id(bp)
+
+def check_id(id_list):
+    min_id = min(id_list)
+    max_id = max(id_list)
+    for x in range(min_id, max_id):
+        if x not in id_list:
+            return x
+
+print("My seat ID is:",check_id(seat_ids))
